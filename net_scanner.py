a4748870bdcb8501efc52fdd19de9fe408e5d188
Tumbler <95390026+Tumbler-exe@users.noreply.github.com>
2023-11-01 21:04:48 +0300

    Add files via upload

---
+++ net_scanner.py
@@ -0,0 +1,39 @@
+import scapy.all as scapy
+from optparse import OptionParser
+
+#1)arp request
+#2)broadcast
+#3)response
+
+def get_user_input():
+    parse_object = OptionParser()
+    parse_object.add_option("-i","--ipaddress",dest="ip_address",help="Enter IP Address")
+
+    (user_input,arguments) = parse_object.parse_args()
+
+    if not user_input.ip_address:
+        print("Enter IP Address: ")
+
+    return user_input
+
+
+def scan_my_network(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)
+    #scapy.ls(scapy.ARP)
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    combined_packet = broadcast_packet/arp_request_packet
+
+    (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1)
+    answered_list.summary()
+
+print("\nNet Scanner Started!\n")
+
+user_ip_address = get_user_input()
+scan_my_network(user_ip_address.ip_address)
+
+print("\nNet Scanner Finished Successfully :) ")
+
+
+
+
